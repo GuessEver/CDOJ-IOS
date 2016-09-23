ad9acfe88c3fb6f883485690dc16c99ed388788f
GuessEver <guessever@gmail.com>
2016-09-23 14:07:32 +0800

    update brief content

--- CDOJ-IOS/Function/String.m
+++ CDOJ-IOS/Function/String.m
@@ -10,16 +10,35 @@
 
 @implementation String
 
++ (NSString*)replaceString:(NSString*)str withRegexPattern:(NSString*)pattern toTarget:(NSString*)target {
+    NSRegularExpression* regex = [NSRegularExpression regularExpressionWithPattern:pattern options:0 error:nil];
+    return [regex stringByReplacingMatchesInString:str options:0 range:NSMakeRange(0, str.length) withTemplate:target];
+}
+
 NSString* convertToBriefContent(NSString* str) {
     // replace extra line break (\n) due to markdown
-    NSRegularExpression* regex = [NSRegularExpression regularExpressionWithPattern:@"(\n)(\n)+" options:0 error:nil];
-    str = [regex stringByReplacingMatchesInString:str options:0 range:NSMakeRange(0, str.length) withTemplate:@"\n"];
-    // replace picture link (markdown) to a simple tag
-    regex = [NSRegularExpression regularExpressionWithPattern:@"!\\[.*?\\]\\(.*?\\)" options:0 error:nil];
-    str = [regex stringByReplacingMatchesInString:str options:0 range:NSMakeRange(0, str.length) withTemplate:@"[图片]"];
+    str = [String replaceString:str withRegexPattern:@"(\n)(\n)+" toTarget:@"\n"];
+    
+    // replace picture (markdown) to a simple tag
+    str = [String replaceString:str withRegexPattern:@"!\\[.*?\\]\\(.*?\\)" toTarget:@"[图片]"];
+    // replace picture (html) to a simple tag
+    str = [String replaceString:str withRegexPattern:@"<img.*?>" toTarget:@"[图片]"];
+    
+    // link (markdown)
+    str = [String replaceString:str withRegexPattern:@"\\[(.*?)\\]\\(.*?\\)" toTarget:@"$1"];
+    // link (html)
+    str = [String replaceString:str withRegexPattern:@"<a.*?>(.*?)</a>" toTarget:@"$1"];
+    
+    // mathjax marks
+    str = [String replaceString:str withRegexPattern:@"\\$\\$?(.*?)\\$\\$?" toTarget:@"$1"];
+    
+    // remove code(markdown) marks
+    str = [String replaceString:str withRegexPattern:@"`(``)?" toTarget:@""];
+    
     // replace H* titles mark
-    regex = [NSRegularExpression regularExpressionWithPattern:@"^(#)+( )*" options:0 error:nil];
-    str = [regex stringByReplacingMatchesInString:str options:0 range:NSMakeRange(0, str.length) withTemplate:@""];
+    str = [String replaceString:str withRegexPattern:@"^(#)+( )*" toTarget:@""];
+    // remove all html tags
+    str = [String replaceString:str withRegexPattern:@"<.*?>" toTarget:@""];
     return str;
 }
 
